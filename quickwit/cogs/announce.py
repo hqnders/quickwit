5c55127defa5245862c080957985271c252bb16e
Henri Kuunders <henrikuunders@hotmail.com>
2024-12-05 16:11:59 +0100

    Fixed announcement command

--- quickwit/cogs/announce.py
+++ quickwit/cogs/announce.py
@@ -21,13 +21,20 @@ class Announce(commands.Cog):
             message (str): The announcement to make
         """
         storage_cog = self.bot.get_cog('Storage')  # type: storage.Storage
+
         stored_event = storage_cog.get_event(interaction.channel_id)
         if stored_event is None:
             await interaction.response.send_message(content="Could not find event associated with this channel", ephemeral=True)
             return
+
+        if interaction.user.id != stored_event.event.organiser_id:
+            await interaction.response.send_message(content="Only the event organiser may make announcements", ephemeral=True)
+            return
+
         message = MESSAGE_FORMAT.format(
             organiser=stored_event.event.organiser_id, message=message)
         for user_id in stored_event.event.registrations.keys():
             if user_id != stored_event.event.organiser_id:
                 message += f'<@{user_id}>'
         await interaction.channel.send(message)
+        await interaction.response.send_message(content="Announcement has been made", ephemeral=True)
